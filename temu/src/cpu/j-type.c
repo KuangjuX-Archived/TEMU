8d829fa78ca8cc09023cb0b67fa12f10cfba4fa4
BenchBeach <1779227906@qq.com>
2021-10-19 15:31:07 +0800

    feat: jal

--- temu/src/cpu/j-type.c
+++ temu/src/cpu/j-type.c
@@ -13,15 +13,14 @@ static void decode_j_type(uint32_t instr) {
 make_helper(j) {
     decode_j_type(instr);
     uint32_t addr = (((int)cpu.pc)&0xF0000000) | (op_src1->instr_index << 2);
-    cpu.pc = addr - 4 ; // why
+    cpu.pc = addr - 4 ;
     sprintf(assembly, "J %x", cpu.pc+4);
 }
 
 make_helper(jal) {
-    //TODO test
     decode_j_type(instr);
     cpu.ra=cpu.pc+8;
     uint32_t addr = (((int)cpu.pc)&0xF0000000) | (op_src1->instr_index << 2);
-    cpu.pc = addr - 4 ; // why
+    cpu.pc = addr - 4 ;
     sprintf(assembly, "JAL %x", cpu.pc+4);
 }
