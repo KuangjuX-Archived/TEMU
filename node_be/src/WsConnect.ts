0c7aa8c7ed6fd5770689f6321ed0f916f07bc615
BenchBeach <1779227906@qq.com>
2021-10-26 17:11:17 +0800

    feat finish back end

--- node_be/src/WsConnect.ts
+++ node_be/src/WsConnect.ts
@@ -2,7 +2,7 @@ import { WebSocketServer, WebSocket} from 'ws';
 import TemuConnect from "./TemuConnect";
 type RawData=Buffer | ArrayBuffer | Buffer[]
 
-enum wsStatus {UNREADY,READY}
+enum wsStatus {UNREADY,READY,CLOSE}
 
 class WsConnect {
     public constructor(){
@@ -25,17 +25,25 @@ class WsConnect {
     private handleOnConnect(){
         console.log("connected")
         this.status=wsStatus.READY;
+        this.temuC=new TemuConnect()
+        this.temuC.setWsC(this)
     }
-    private static recvFromClient(message:RawData){
+    private recvFromClient(message:RawData){
         console.log('received: %s', message);
+        this.temuC?.sendMsg2Temu(message.toString())
     }
     public init(port:number){
         this.wsServer=new WebSocketServer({ port: port });
         this.wsServer.on('connection', (ws)=>{
             this.handleOnConnect()
             this.ws=ws;
-            this.ws.on('message', WsConnect.recvFromClient)
+            this.ws.on('message', this.recvFromClient)
+            this.ws.on('close',()=>{
+                this.status=wsStatus.CLOSE
+                this.temuC=null
+            })
         });
+        console.log("[WS] init successfully.")
     }
 
     public send2Client(msg:string){
--- node_be/src/index.ts
+++ node_be/src/index.ts
@@ -5,11 +5,6 @@ import TemuConnect from "./TemuConnect";
 function main(config:Config):void{
     let myWsConnect=new WsConnect()
     myWsConnect.init(config.port)
-    let myTemuConnect=new TemuConnect()
-    myTemuConnect.setWsC(myWsConnect)
-    myWsConnect.setTemuC(myTemuConnect)
-    setTimeout(()=>{myTemuConnect.sendMsg2Temu("si 1")},15000)
 }
 
 main(myConfig)
-console.log("stated")
